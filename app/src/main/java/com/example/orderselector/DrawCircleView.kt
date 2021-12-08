a42f2db02a6153f1b009f4fe6be08e1301908bf7
domerka <dcsulits@gmail.com>
2021-12-08 15:55:17 +0100

    Ordering finished!

---
+++ app/src/main/java/com/example/orderselector/DrawCircleView.kt
@@ -0,0 +1,141 @@
+package com.example.orderselector
+
+import android.content.Context
+import android.graphics.Canvas
+import android.graphics.Color
+import android.graphics.Paint
+import android.graphics.PointF
+import android.os.CountDownTimer
+import android.util.AttributeSet
+import android.view.MotionEvent
+import android.view.View
+import android.widget.Toast
+
+
+class DrawCircleView(context: Context, attrs: AttributeSet) : View(context, attrs) {
+
+    private val colors = intArrayOf(
+    Color.BLUE, Color.GREEN, Color.MAGENTA,
+    Color.WHITE, Color.CYAN, Color.GRAY, Color.RED, Color.DKGRAY,
+    Color.LTGRAY, Color.YELLOW
+    )
+
+    private var mActivePointers: Array<Boolean> = Array(10){false}
+    private val paint : Paint = Paint()
+    private val textPaint : Paint = Paint()
+    private var isTimerDone : Boolean = false
+
+    private var positions : Array<PointF> = Array(10){ PointF() }
+
+    private val randoNumbers: MutableList<Int> = mutableListOf()
+
+    private var numberOfPointers = 0
+
+    private var timer : CountDownTimer = object: CountDownTimer(0,0){
+        override fun onTick(millisUntilFinished: Long) {
+        }
+        override fun onFinish() {
+        }
+    }
+
+
+    override fun onTouchEvent(event: MotionEvent): Boolean {
+
+        val pointerIndex : Int = event.actionIndex
+        val pointerId : Int = event.getPointerId(pointerIndex)
+
+        when(event.actionMasked){
+            MotionEvent.ACTION_DOWN -> {
+                if(isTimerDone){
+                    randoNumbers.clear()
+                    numberOfPointers = 0
+                    positions = Array(10){ PointF() }
+                    mActivePointers = Array(10){false}
+                    isTimerDone = false
+                }
+
+                val f = PointF()
+                f.x = event.getX(pointerIndex)
+                f.y = event.getY(pointerIndex)
+                mActivePointers[pointerId] = true
+                positions[pointerId] = f
+                timer = object : CountDownTimer(3000, 1000) {
+                    override fun onTick(millisUntilFinished: Long) {
+                    }
+                    override fun onFinish() {
+                        isTimerDone = true
+                        invalidate()
+                        Toast.makeText(context,"Finish!",Toast.LENGTH_SHORT).show()
+                    }
+                }.start()
+                numberOfPointers++
+            }
+            MotionEvent.ACTION_POINTER_DOWN ->{
+                val f = PointF()
+                f.x = event.getX(pointerIndex)
+                f.y = event.getY(pointerIndex)
+                mActivePointers[pointerId] = true
+                positions[pointerId] = f
+                numberOfPointers++
+            }
+            MotionEvent.ACTION_MOVE -> {
+                if(!isTimerDone){
+                    val size = event.pointerCount
+                    var i = 0
+                    while (i < size) {
+                        val point = positions[event.getPointerId(i)]
+                        point.x = event.getX(i)
+                        point.y = event.getY(i)
+                        i++
+                    }
+                }
+            }
+            MotionEvent.ACTION_UP ->{
+                mActivePointers[pointerId] = false
+                numberOfPointers--
+            }
+            MotionEvent.ACTION_POINTER_UP ->{
+                mActivePointers[pointerId] = false
+                numberOfPointers--
+                //Toast.makeText(context,test.size.toString(),Toast.LENGTH_SHORT).show()
+            }
+            MotionEvent.ACTION_CANCEL -> {
+                mActivePointers[pointerId] = false
+            }
+        }
+        if(!isTimerDone) invalidate()
+        if(numberOfPointers == 0) timer.cancel()
+        return true
+    }
+
+    override fun onDraw(canvas: Canvas?) {
+        super.onDraw(canvas)
+        textPaint.textSize = 100f
+        textPaint.color = Color.WHITE
+        canvas?.drawColor(Color.BLACK)
+        var k = 0
+        if(isTimerDone){
+            while (k < numberOfPointers){
+                randoNumbers.add(++k)
+                print(k.toString())
+            }
+            randoNumbers.shuffle()
+            k = 0
+        }
+        var i = 0
+        while (i < 10) {
+            if(mActivePointers[i]){
+                paint.color = colors[i]
+                val point: PointF = positions[i]
+                canvas?.drawCircle(point.x, point.y, 100F, paint)
+                if(isTimerDone){
+                    canvas?.drawText(randoNumbers[k].toString(),point.x+90f,point.y,textPaint)
+                    k++
+                }
+            }
+            i++
+        }
+
+    }
+
+}
--- app/src/main/java/com/example/orderselector/Order.kt
+++ app/src/main/java/com/example/orderselector/Order.kt
@@ -16,67 +16,6 @@ class Order : AppCompatActivity()  {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_order)
-
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
     }
-
-}
-
-class DrawCircleView @JvmOverloads constructor(
-    context: Context, attrs: AttributeSet? = null, defStyleAttr: Int = 0
-                    ) : View(context, attrs, defStyleAttr) {
-    private val colors = intArrayOf(
-        Color.BLUE, Color.GREEN, Color.MAGENTA,
-        Color.BLACK, Color.CYAN, Color.GRAY, Color.RED, Color.DKGRAY,
-        Color.LTGRAY, Color.YELLOW
-    )
-    private val pointers: MutableList<PointF> = mutableListOf()
-
-    override fun onTouchEvent(event: MotionEvent): Boolean {
-
-        val pointerIndex : Int = event.actionIndex
-        val pointerId : Int = event.getPointerId(pointerIndex)
-        when(event.actionMasked){
-            MotionEvent.ACTION_DOWN -> {
-            }
-            MotionEvent.ACTION_UP ->{
-            }
-            MotionEvent.ACTION_POINTER_DOWN ->{
-                val f = PointF()
-                f.x = event.getX(pointerIndex)
-                f.y = event.getY(pointerIndex)
-                pointers.add(pointerId, f)
-            }
-            MotionEvent.ACTION_MOVE -> {
-                val size = event.pointerCount
-                var i = 0
-                while (i < size) {
-                    val point: PointF = pointers[event.getPointerId(i)]
-                    point.x = event.getX(i)
-                    point.y = event.getY(i)
-                    i++
-                }
-            }
-            MotionEvent.ACTION_POINTER_UP ->{
-                pointers.removeAt(pointerId)
-            }/*
-            MotionEvent.ACTION_CANCEL -> {
-                pointers.removeAt(pointerId);
-            }*/
-        }
-        return true
-    }
-    override fun onDraw(canvas: Canvas?) {
-        super.onDraw(canvas)
-        val size: Int = pointers.size
-        var i = 0
-        while (i < size) {
-            val point: PointF = pointers[i]
-            if (canvas != null) {
-                canvas.drawCircle(point.x, point.y, 10F, Paint())
-            }
-            i++
-        }
-    }
-
 }
